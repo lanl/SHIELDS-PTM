1bc5ed879b89d1eca2bc6c18e31c83345e471ffb
Jesse Richard Woodroffe <jwoodroffe@lanl.gov>
2016-06-07 16:48:06 -0600

    Added documentation to ptm_input.py

--- scripts/ptm_input.py
+++ scripts/ptm_input.py
@@ -76,7 +76,10 @@ def get_default_inputs(ivelocity=1,idensity=1):
   | OUTPUTS |
   -----------
   
-  None. Three text files are created but the routine returns a null value.
+  ptmdict: Dictionary
+  
+  ptmdict has numerous keys that correspond to different parameters used to configure the ptm simulation. See
+  the code itself for descriptions of individual keys.
   
   * * * * * * * * * *
   
@@ -87,28 +90,27 @@ def get_default_inputs(ivelocity=1,idensity=1):
   
   """
 
-
   # Default parameters for ptm_parameters file
   pdict={}
   pdict['runid']=1
-  pdict['nparticles']=1
-  pdict['ndim']=3
-  pdict['nx']=75
-  pdict['ny']=75
-  pdict['nz']=75
-  pdict['itrace']=-1
-  pdict['ifirst']=1
-  pdict['ilast']=2
-  pdict['dtin']=3600.0
-  pdict['dtout']=1.0
-  pdict['istep']=1
-  pdict['iswitch']=-1
-  pdict['iphase']=3
-  pdict['nphase']=32
-  pdict['charge']=-1.0
-  pdict['mass']=1.0
-  pdict['tlo']=0.0
-  pdict['thi']=3600.0
+  pdict['nparticles']=1   # Number of particles to use in simulation
+  pdict['ndim']=3         # Number of spatial dimensions of input data (2 or 3)
+  pdict['nx']=75          # Number of points in X grid
+  pdict['ny']=75          # Number of points in Y grid
+  pdict['nz']=75          # Number of points in Z grid
+  pdict['itrace']=-1      # 1 = forward in time, -1 = backwards in time
+  pdict['ifirst']=1       # Timestep of first fields snapshot: e.g., start at bx3d_0005.bin if ifirst=5
+  pdict['ilast']=2        # Timestep of last fields snapshot: e.g., finish at bx3d_0009.bin if ilast=9
+  pdict['dtin']=3600.0    # Time between snapshots (seconds). If dtin<0, read times from tgrid.bin
+  pdict['dtout']=1.0      # Time between data outputs (seconds)
+  pdict['istep']=1        # Integrator to use: 1=RK4, 2=Adaptive RKSuite
+  pdict['iswitch']=-1     # Particle equations to solve: -1=guiding center, 0=dynamic switching, 1=full orbit
+  pdict['iphase']=3       # Gyrophase switching method: 1=random, 2=brute force search, 3=gradient method
+  pdict['nphase']=32      # Number of points for brute force search (minimize difference in B)
+  pdict['charge']=-1.0    # Particle charge in multiples of fundamental
+  pdict['mass']=1.0       # Particle mass in multiples of the electron mass
+  pdict['tlo']=0.0        # Lowest time of particle trace
+  pdict['thi']=3600.0     # Highest time of particle trace
   
   # Default parameters for velocity file
   vdict={}
@@ -117,25 +119,25 @@ def get_default_inputs(ivelocity=1,idensity=1):
   if(ivelocity==1):
     # Beam
     vdict['idist']=1
-    vdict['ekev']=100.0
-    vdict['alpha']=90.0
-    vdict['phi']=180.0
+    vdict['ekev']=100.0     # Particle energy in keV
+    vdict['alpha']=90.0     # Particle pitch angle in degrees
+    vdict['phi']=180.0      # Particle gyrophase angle in degrees; set negative to seed randomly
   elif(ivelocity==2):
     # Maxwellian
     vdict['idist']=2
-    vdict['vtperp']=0.25*ckm
-    vdict['vtpara']=0.25*ckm
-    vdict['phi']=180.0  
+    vdict['vtperp']=0.25*ckm  # Perpendicular thermal velocity in km/s
+    vdict['vtpara']=0.25*ckm  # Parallel thermal velocity in km/s
+    vdict['phi']=180.0        # Gyrophase angle in degrees; set negative to seed randomly
   elif(ivelocity==3):
     # Flux map mode
     vdict['idist']=3
-    vdict['nenergy']=34
-    vdict['npitch']=31
-    vdict['phi']=180.0
-    vdict['emin']=1.0
-    vdict['emax']=100.0
-    vdict['pamin']=0.0
-    vdict['pamax']=90.0
+    vdict['nenergy']=34       # Number of energies to use in discrete flux map
+    vdict['npitch']=31        # Number of pitch angles to use in discrete flux map
+    vdict['phi']=180.0        # Gyrophase angle in degrees; set negative to seed randomly
+    vdict['emin']=1.0         # Lowest energy of flux map (keV)
+    vdict['emax']=100.0       # Highest energy of flux map (keV)
+    vdict['pamin']=0.0        # Lowest pitch angle of flux map (degrees)
+    vdict['pamax']=90.0       # Highest pitch angle of flux map (degrees)
   else:
     raise Exception('Option ivelocity = '+str(ivelocity)+' is not supported.')
          
@@ -144,24 +146,24 @@ def get_default_inputs(ivelocity=1,idensity=1):
   if(idensity==1):
     # Single point
     ddict['idens']=1
-    ddict['x0']=-5.0
-    ddict['y0']=0.0
-    ddict['z0']=0.0 
+    ddict['x0']=-5.0      # Initial X-position (RE)
+    ddict['y0']=0.0       # Initial Y-position (RE)
+    ddict['z0']=0.0       # Initial Z-position (RE)
   elif(idensity==2):
     # Cubic region
     ddict['idensity']=2
-    ddict['xmin']=-7.0
-    ddict['xmax']=-6.0
-    ddict['ymin']=-0.5
-    ddict['ymax']= 0.5
-    ddict['zmin']=-0.5
-    ddict['zmax']=0.5
+    ddict['xmin']=-7.0    # Minimum X-boundary of seeding region (RE)
+    ddict['xmax']=-6.0    # Maximum X-boundary of seeding region (RE)
+    ddict['ymin']=-0.5    # Minimum Y-boundary of seeding region (RE)
+    ddict['ymax']= 0.5    # Maximum Y-boundary of seeding region (RE)
+    ddict['zmin']=-0.5    # Minimum Z-boundary of seeding region (RE)
+    ddict['zmax']=0.5     # Maximum Z-boundary of seeding region (RE)
   elif(idensity==3):
     # Radial distance
     ddict['idens']=3
-    ddict['r0']=6.6
-    ddict['mltmin']=-6.0
-    ddict['mltmax']=6.0    
+    ddict['r0']=6.6       # Radial distance for seeding (RE)
+    ddict['mltmin']=-6.0  # Minimum local time for seeding region (HOURS)
+    ddict['mltmax']=6.0   # Maximum local time for seeding region (HOURS)
   
   ptmdict={'p':pdict,'v':vdict,'d':ddict}
 
@@ -169,12 +171,48 @@ def get_default_inputs(ivelocity=1,idensity=1):
 
 def create_input_files(pd,filedir=''):
   """
-  Use the user-provided parameter dictionary to write the ptm input files.
+  Use a user-provided parameter dictionary to write the ptm input files. The get_default_inputs routine can be used
+  to get a template for this dictionary, and individual components can be modified before passing that dictionary
+  to this routine.
+  
+  * * * * * * * * * *
+  ----------
+  | INPUTS |
+  ----------  
+  
+  pd    Dictionary    Contains specification of all the parameters for creation of ptm input files:
+    
+  1. ptm_parameters_xxxx.dat
+  2. dist_velocity_xxxx.dat
+  3. dist_density_xxxx.dat
+  
+  filedir   String  Optional    Directory (relative or absolute) where output files should be written.
+  
+  See description of individual parameters in <get_default_inputs>
+
+  -----------
+  | OUTPUTS |
+  -----------  
+  
+   None. Three text files are created but the routine returns a null value.
+
+  * * * * * * * * * * 
   
   Jesse Woodroffe
   6/7/2016
   """
 
+  # Handle a couple of special cases
+  if(len(filedir)>0):
+    if(fileir=='.'):
+      # User specified present directory using dot; strip the dot 
+      fileir=''
+    elif(searchDir[-1]!='/'): 
+      # User forgot to add a slash at the end of the directory name; add a slash
+      filedir+='/'
+
+  # Write the parameters file, ptm_parameters_xxxx.dat
+  
   pfile=open(filedir+'ptm_parameters_%4.4i.txt' % pd['p']['runid'],'w')
   pfile.write('%-8i \t\t\t\t RunID this needs to match the 4 digit tag (without leading zeros)\n' % pd['p']['runid'])
   pfile.write('%-8i \t\t\t\t Number of particles (value is overridden for dist_velocity::idist==3)\n' % pd['p']['nparticles'])
@@ -197,11 +235,12 @@ def create_input_files(pd,filedir=''):
   pfile.write('%-8.1f \t\t\t\t upper limit of time integration (s)\n' % pd['p']['thi'])
   pfile.close()
 
+  # Write the density file, dist_density_xxxx.dat
+
   dfile=open(filedir+'dist_density_%4.4i.txt' % pd['p']['runid'],'w')
   dfile.write('%-8i \t\t\t\t idens\n' % pd['d']['idens'])
   
   if(pd['d']['idens']==1):
-    # Write dist_density file
     dfile.write('%-8.2f \t\t\t\t Initial X position (Re)\n' % pd['d']['x0'])
     dfile.write('%-8.2f \t\t\t\t Initial Y position (Re)\n' % pd['d']['y0'])
     dfile.write('%-8.2f \t\t\t\t Initial Z position (Re)\n' % pd['d']['z0'])
@@ -221,7 +260,8 @@ def create_input_files(pd,filedir=''):
   
   dfile.close()
 
-  # Write dist_velocity file
+  # Write velocity distribution function file, dist_velocity_xxxx.dat
+
   vfile=open(filedir+'dist_velocity_%4.4i.txt' % pd['p']['runid'],'w')
   vfile.write('%-8i \t\t\t\t idist\n' % pd['v']['idist'])
   if(pd['v']['idist']==1):
